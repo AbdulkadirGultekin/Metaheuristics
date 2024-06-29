8ba079a0792e8e37ee61c4041634c0d85ea0b69c
AbdulkadirGultekin <gultekinn.abdulkadir@gmail.com>
2024-06-29 13:27:52 +0300

    Delete Obj_value_calculate.py

--- Obj_value_calculate.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def calc_obj(obj_coeffs, takens,weights,capacity):
-    value_vector = np.multiply(obj_coeffs, takens)
-    weight_vector = np.multiply(weights, takens)
-    tot_weight = weight_vector.sum()
-    if tot_weight <= capacity:
-        return value_vector.sum()
-    else:
-        return -np.inf
