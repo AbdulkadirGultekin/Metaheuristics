3ce7ca585ed675c9f028b7b49f71b22880701c4f
AbdulkadirGultekin <gultekinn.abdulkadir@gmail.com>
2024-06-29 13:27:23 +0300

    Delete Find_neighbours.py

--- Find_neighbours.py
+++
@@ -1,8 +0,0 @@
-from random import randrange
-import numpy as np
-def find_neighbours(vector):
-    flip_idx = randrange(len(vector))
-    neighbour = np.array(vector)
-    neighbour[flip_idx] = 1 - neighbour[flip_idx]
-    return neighbour
-
